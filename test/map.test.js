7cec0b6ecb511f353fd6a299da154c95c6f998e2
Adrien <adrien@arccoza.com>
2019-02-20 15:35:24 +0900

    Rename tap import to test

--- test/map.test.js
+++ test/map.test.js
@@ -1,8 +1,8 @@
-const t = require('tape')
+const test = require('tape')
 const map = require('../src/map')
 
 
-t('Each iter item should equal original value modified by fn', function (t) {
+test('Each iter item should equal original value modified by fn', function (t) {
   var arr = [-2,-1,0,1,2,3,4], i = 0
   var fn = v => 2 * v
   var mapIt = map(fn, arr)
