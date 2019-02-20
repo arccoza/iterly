f2f88db25c8ee2242cbcc48c78b27fc905cf85dd
Adrien <adrien@arccoza.com>
2019-02-20 16:41:23 +0900

    Update map test

--- test/map.test.js
+++ test/map.test.js
@@ -1,6 +1,7 @@
 const test = require('tape')
 const map = require('../src/map')
 
+
 test('map', t => {
   t.test('Should have the same items, modified by fn, in the same order as original', function (t) {
     var arr = [-2,-1,0,1,2,3,4], i = 0
@@ -11,22 +12,8 @@ test('map', t => {
       t.equal(v, fn(arr[i]))
       i++
     }
-    
-    t.end()
-  })
 
-  t.test('Should have the same number of items as original', function (t) {
-    var arr = [1,2,3,4], res
-    var fn = v => v
-    var it = map(fn, arr)
-
-    it.next()
-    it.next()
-    it.next()
-    it.next()
-    res = it.next()
-
-    t.ok(res.done)
+    t.equal(i, arr.length, 'should have the same number of items as original')
     
     t.end()
   })
