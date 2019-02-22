dabeaf6f9ea8dad44953e38a6953da4c9a6bd084
Adrien <adrien@arccoza.com>
2019-02-22 15:12:32 +0900

    Add index file, exports all public objects

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const map = require('./map')
+const filter = require('./filter')
+const reduce = require('./reduce')
+const range = require('./range')
+const enumerate = require('./enumerate')
+const amap = require('./amap')
+const afilter = require('./afilter')
+const areduce = require('./areduce')
+const {ITERATOR, ASYNC_ITERATOR, setIt, iter, iterArray, iterObject, iterFn, iterReadable} = require('./iter')
+const {isFunction, isPromise, isAsyncIter, curry, compose, toAsync, each, anext} = require('./tools')
+
+
+module.exports = {
+  map, filter, reduce,
+  range, enumerate,
+  amap, afilter, areduce,
+  ITERATOR, ASYNC_ITERATOR, setIt, iter, iterArray, iterObject, iterFn, iterReadable,
+  isFunction, isPromise, isAsyncIter, curry, compose, toAsync, each, anext,
+}
