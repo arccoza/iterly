6e9c6c48eb2b5dc80eae068b0833271b55fd467f
Adrien <adrien@arccoza.com>
2019-02-23 21:46:03 +0900

    Add docs

--- src/map.js
+++ src/map.js
@@ -4,6 +4,15 @@ const {curry} = require('./tools')
 const {setIt, iter} = require('./iter')
 
 
+/**
+ * A function that maps iterator values over a function.
+ * @example
+ * var it = map(v => 2 * v, [1, 2, 3, 4])
+ * // Will give you an iterator with the values: 2, 4, 6, 8
+ * @param {function(value:any, index:number)} fn - The function to modify each value in the iterator.
+ * @param {@@iterator} it - The iterator or iterable to map over.
+ * @returns {@@iterator} - Returns an iterator with values that will be updated by fn.
+ */
 function map(fn, it) {
   it = iter(it)
   
