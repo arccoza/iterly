dda1775c9934244151dfdb747687085d769e655b
Adrien <adrien@arccoza.com>
2019-02-23 21:55:16 +0900

    Add docs

--- src/filter.js
+++ src/filter.js
@@ -4,6 +4,16 @@ const {curry} = require('./tools')
 const {setIt, iter} = require('./iter')
 
 
+/**
+ * A function that keeps values in an iterator or iterable only if fn returns true.
+ * @example
+ * var it = filter(v => v != 2, [1, 2, 3, 4])
+ * // Will give you an iterator with the values: 1, 3, 4
+ * @param {function(value:any, index:number)} fn - The test function to filter items,
+ * return true to keep the item, false to remove it.
+ * @param {@@iterator} it - The iterator or iterable to filter.
+ * @returns {@@iterator} - Returns an iterator with only the values that passed the test fn.
+ */
 function filter(fn, it) {
   it = iter(it)
   
