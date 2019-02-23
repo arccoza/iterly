d8608097ef250aa59478d6850e43237e10fdbb2e
Adrien <adrien@arccoza.com>
2019-02-23 22:06:52 +0900

    Add docs

--- src/reduce.js
+++ src/reduce.js
@@ -4,6 +4,19 @@ const {curry} = require('./tools')
 const {setIt, iter} = require('./iter')
 
 
+/**
+ * A function that reduces all the items in an iterator or iterable to one value.
+ * @example
+ * var it = reduce((acc, v) => acc += v, 0, [1, 2, 3, 4])
+ * // Will give you an iterator with the value: 10
+ * var it = reduce((acc, v, i) => (acc[i] = v, acc), [], [1, 2, 3, 4])
+ * // Will give you an iterator with a single item, which will be an array: [1, 2, 3, 4]
+ * @param {function(value:any, index:number)} fn - The reducer function continually updates
+ * the accumulator, until the iterator ends, then the accumulated value is returned.
+ * @param acc - The accumulator, each item in the iterator or iterable will be added to this by fn.
+ * @param {@@iterator} it - The iterator or iterable to reduce.
+ * @returns {@@iterator} - Returns an iterator with only one value.
+ */
 function reduce(fn, acc, it) {
   it = iter(it)
   var value, done
